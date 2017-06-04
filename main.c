9063437cf94580a9a2dd55e760a1bea83ae762e7
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-04 14:18:53 -0400

    Main Visual and Engine Overhaul
    Added proper addition. Clickers give 1 cookie per second, grandmas give two. Bakeries (or factories, I have zero idea what to call them) give 5.
    
    A new title screen and all. Proper displays and no uglification. I'm trying to figure out pages so I can have multiple pages.

--- main.c
+++ main.c
@@ -31,12 +31,6 @@ int main()
 	//Choose which screen to print to (top)
 	consoleSelect(&topScreen);
 
-	;printf("Hello, this is a cookie tapper app, by @Kaisogen!\n\n"); //For some reason the code won't compile without this semicolon at the beginning.
-	printf("Get more cookies by pressing A!\nFor a longer tutorial, press B!\n\n"); 
-	printf("If you want to see bugfixes for the current version, press X!\n\n");
-	printf("This is version 1.2!\n\n\n");
-	
-
 	// Main loop
 	while (aptMainLoop())
 	{
@@ -49,16 +43,17 @@ int main()
 		if (kDown & KEY_START)
 			break; //Break in order to return to hbmenu
 		
-		//Tutorial!
-		if(kDown & KEY_B){	//kDown is true when a key is held down.
-			consoleClear();
-			printf("\n\n\n Hey there! Welcome to Cookie Collector! Thanks for opening the advanced explanation!");
-			printf("\nIf you press A, you get your cookie production added to your total cookies!");
-			printf("\nIf you press Up on the D-Pad, you can buy a clicker for a certain amount of cookies. This adds 0.1 cookies to your total cookies produced per click.");
-			printf("\nPress Right on the D-Pad for Bakeries. They add 100!");
-			printf("\nPress Left to hire a grandma. They give you one extra cookie per second!");
-			printf("Press the Home button if you ever want to leave!");
-		}
+		
+		
+		// Title Screen
+		printf("\x1b[1;14HCookie Collector 3DS");
+		printf("\x1b[5;2HYou have: %d cookies", cookies);
+		printf("\x1b[6;0H__________________________________________________");
+		printf("\x1b[8;2HIt costs %d cookies to build a clicker", clickCost);
+		printf("\x1b[9;2HIt costs %d cookies to hire a grandma", grandmaCost);
+		printf("\x1b[10;2HIt costs %d cookies to build a bakery", bakeryCost);
+		
+		
 		
 	
 		//Add Cookies	
@@ -74,11 +69,6 @@ int main()
 				cookies-=clickCost;
 				clickCost = clickCost * 1.5;
 				clickers++;
-			} else {
-				consoleSelect(&bottomScreen);
-				consoleClear();
-				printf("You don't have enough cookies to buy a clicker!\n");
-				printf("You need %d more cookies!\n", clickCost - cookies);
 			}
 		}
 		
@@ -88,11 +78,6 @@ int main()
 				cookies-=grandmaCost;
 				grandmaCost = grandmaCost * 1.5;
 				grandmas++;
-			} else {
-				consoleSelect(&bottomScreen);
-				consoleClear();
-				printf("You don't have enough cookies to hire a grandma!\n");
-				printf("You need %d more cookies!\n", grandmaCost - cookies);
 			}
 		}
 		
@@ -101,43 +86,33 @@ int main()
 				cookies-=bakeryCost;
 				bakeryCost = bakeryCost * 1.5;
 				bakeries++;
-			} else {
-				consoleSelect(&bottomScreen);
-				consoleClear();
-				printf("You don't have enough cookies to construct a bakery!\n");
-				printf("You need %d more cookies!\n", bakeryCost - cookies);
-			}
+			} 
 		}
-		
-		//Version Revisions
-		if (kDown & KEY_X){
-			consoleClear();
-			printf("V.1.2: \n    1. Removed the cookie multiplier! \n    2. Added new ways to get more cookies!\n    Press Up to get a clicker, left to hire a grandma, and right to build a factory!\n    3. Statistics are now displayed on the bottomscreen, info on top!");
-		}	
 		
 		consoleSelect(&bottomScreen);
-		//I couldn't get a timer working properly, so sorry for stealing your code, Machinumps! 
-		//This timer checks if the remainder of the frames value, when divided by three, is zero. If the remainder is zero, it fires.
-		//This should be firing every free times then.
-		if (frames % 3 == 0) {
-			consoleClear();
-			cookies = cookies + (clickers * 0.05);
-			cookies = cookies + (grandmas * 0.15);
-			cookies = cookies + (factories * 5);
+
+		frames++;
+		
+		if (frames % 60 == 0) 
+		{
+			cookies = cookies + clickers;
+			
+			cookies = cookies + grandmas * 2;
+			
+			frames = 0;
 		}
 		
-		//Lazy reset frames. I haven't tested this code yet, so it might not work. I gave it three seconds before resetting.
-		//(dev comment) Old internal revisions had it resetting at exactly three seconds. In case the timer missed the three second mark by just a little bit, I added a proper operation to avoid this.
-		if (frames % 180 == 0) {
-			frames = 0;	
-		}
+		consoleClear();
 		
 		//%d is a replacement for an integer
-		printf("You have %d cookies!\n", cookies);
-		printf("You gain %d cookies per round!\n", cookieM);
-		printf("You have %d clickers so far!\n", clickers);
-		printf("You have %d grandmas hired so far!\n", grandmas);
-		printf("You have %d bakeries built so far!\n", bakeries);
+		printf("\x1b[1;1HYou gain %d cookies per click!\n", cookieM);
+		printf("\x1b[3;1HYou have %d clickers so far!\n", clickers);
+		printf("\x1b[4;1HYou have %d grandmas hired so far!\n", grandmas);
+		printf("\x1b[5;1HYou have %d bakeries built so far!\n", bakeries);
+		
+		printf("\x1b[7;1HPress /\\ to buy a clicker");
+		printf("\x1b[8;1HPress <- to hire a grandma");
+		printf("\x1b[9;1HPress -> to produce a factory");
 		// Flush and swap frame-buffers
 		gfxFlushBuffers();
 		gfxSwapBuffers();
