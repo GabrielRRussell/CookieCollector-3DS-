1c8ec30d1cf648132ad31e596db22556e3ab9731
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-05 12:58:26 -0400

    Added Save System
    Big changes, added a brand new saving system.

--- main.c
+++ main.c
@@ -4,31 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long int cookies = 0;
+u64 cookies = 0;
 int cookieM = 1; //Multiplier
-int clickCost = 10; //Automatic Cost
-int grandmaCost = 100; //Automatic Cost
-int bakeryCost = 1000; //Automatic Cost
-
 // Keep track of values
-int clickers = 0;
-int grandmas = 0;
-int bakeries = 0;
+
+u64 buildingData[6] = {0};
 
 //Framerate Check
 int frames = 0; 
 
 int main()
 {
+
+	buildingData[3] = 10;
+	buildingData[4] = 100;
+	buildingData[5] = 1000;
+
 	gfxInitDefault();
 	//Select Screens
 	PrintConsole topScreen, bottomScreen;
 	
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
+	
 	//Choose which screen to print to (top)
 	consoleSelect(&topScreen);
 
+	FILE * fp = fopen("/3ds/data/cookiecollector/test.txt", "r+");
+    fread(&cookies, sizeof(u64), 1, fp);
+	fread(buildingData, sizeof(u64), 5, fp);
+	rewind(fp);
+	
 	// Main loop
 	while (aptMainLoop())
 	{
@@ -45,11 +51,11 @@ int main()
 		
 		// Title Screen
 		printf("\x1b[1;14HCookie Collector 3DS");
-		printf("\x1b[5;15HYou have: %lld cookies", cookies);
+		printf("\x1b[5;15HYou have: %lld cookies \e[K\n", cookies);
 		printf("\x1b[6;0H__________________________________________________");
-		printf("\x1b[8;2HIt costs %d cookies to build a clicker", clickCost);
-		printf("\x1b[9;2HIt costs %d cookies to hire a grandma", grandmaCost);
-		printf("\x1b[10;2HIt costs %d cookies to build a bakery", bakeryCost);
+		printf("\x1b[8;2HIt costs %llu cookies to build a clicker\e[K\n", buildingData[3]);
+		printf("\x1b[9;2HIt costs %llu cookies to hire a grandma\e[K\n", buildingData[4]);
+		printf("\x1b[10;2HIt costs %llu cookies to build a bakery\e[K\n", buildingData[5]);
 		
 		
 		
@@ -62,29 +68,36 @@ int main()
 		//Buy clicker	
 		if (kDown & KEY_UP){
 			
-			if(cookies >= clickCost){
-				cookies-=clickCost;
-				clickCost = clickCost * 1.2;
-				clickers++;
+			if(cookies >= buildingData[3]){
+				cookies-=buildingData[3];
+				buildingData[3] = buildingData[3] * 1.2;
+				buildingData[0]++;
 			}
 		}
 		
 		//Buy grandma
 		if( kDown & KEY_LEFT){
-			if(cookies >= grandmaCost){
-				cookies-=grandmaCost;
-				grandmaCost = grandmaCost * 1.2;
-				grandmas++;
+			if(cookies >= buildingData[4]){
+				cookies-=buildingData[4];
+				buildingData[4] = buildingData[4] * 1.2;
+				buildingData[1]++;
 			}
 		}
 		
 		if(kDown & KEY_RIGHT){
-			if(cookies >= bakeryCost){
-				cookies-=bakeryCost;
-				bakeryCost = bakeryCost * 1.2;
-				bakeries++;
+			if(cookies >= buildingData[5]){
+				cookies-=buildingData[5];
+				buildingData[5] = buildingData[5] * 1.2;
+				buildingData[2]++;
 			} 
 		}
+
+       
+		if (hidKeysDown() & KEY_SELECT) {
+            fwrite(&cookies, sizeof(u64), 1, fp);
+			fwrite(buildingData, sizeof(u64), 5, fp);
+            fclose(fp);
+        }
 		
 		consoleSelect(&bottomScreen);
 
@@ -92,31 +105,31 @@ int main()
 		
 		if (frames % 60 == 0) 
 		{
-			cookies = cookies + clickers;
+			cookies = cookies + buildingData[0];
 			
-			cookies = cookies + grandmas * 2;
+			cookies = cookies + buildingData[1] * 2;
 			
-			cookies = cookies + bakeries * 5;
+			cookies = cookies + buildingData[2] * 5;
 			
 			frames = 0;
+			
 		}
 		
-		consoleClear();
+				printf("\e[K");
 		
 		//%d is a replacement for an integer
-		printf("\x1b[1;1HYou gain %d cookies per click!\n", cookieM);
-		printf("\x1b[3;1HYou have %d clickers so far!\n", clickers);
-		printf("\x1b[4;1HYou have %d grandmas hired so far!\n", grandmas);
-		printf("\x1b[5;1HYou have %d bakeries built so far!\n", bakeries);
+		printf("\x1b[1;1HYou gain %d cookies per click!\e[K\n", cookieM);
+		printf("\x1b[3;1HYou have %llu clickers so far!\e[K\n", buildingData[0]);
+		printf("\x1b[4;1HYou have %llu grandmas hired so far!\e[K\n", buildingData[1]);
+		printf("\x1b[5;1HYou have %llu bakeries built so far!\e[K\n", buildingData[2]);
 		
 		printf("\x1b[7;1HPress /\\ to buy a clicker");
 		printf("\x1b[8;1HPress <- to hire a grandma");
 		printf("\x1b[9;1HPress -> to produce a factory");
+		printf("\x1b[10;1HPress Select to Save");
 		// Flush and swap frame-buffers
 		gfxFlushBuffers();
 		gfxSwapBuffers();
-		
-
 		
 		
 	}
