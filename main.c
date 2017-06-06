c234672d337449b68311299d01086a2643ffafd9
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-05 21:21:13 -0400

    More changes

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 u64 cookies = 0;
 int cookieM = 1; //Multiplier
 int screen = 0;
+int cursor = 12;
 
 // Keep track of values
 
@@ -57,7 +58,7 @@ int main()
 			break; //Break in order to return to hbmenu
 		
 		
-		if (screen = 0) { //Select Which Screen to Use
+		if (screen == 0) { //Select Which Screen to Use
 
 			consoleSelect(&topScreen);
 
@@ -68,15 +69,24 @@ int main()
 			printf("\x1b[8;2HIt costs %llu cookies to build a clicker\e[K\n", buildingData[3]);
 			printf("\x1b[9;2HIt costs %llu cookies to hire a grandma\e[K\n", buildingData[4]);
 			printf("\x1b[10;2HIt costs %llu cookies to build a bakery\e[K\n", buildingData[5]);
+			
+			printf("\x1b[12;5HOption One\e[K\n");
+			printf("\x1b[13;5HOption Two\e[K\n");
+			printf("\x1b[14;5HOption Three (Screen Two)\e[K\n");
+			printf("\x1b[%d;1H>\e[K\n", cursor);
 		
 	
 			//Add Cookies	
 			if (kDown & KEY_A){
 				cookies = cookies + cookieM;
+			} 
+			
+			if (kDown & KEY_A & cursor == 14) {
+				screen = 1;
 			}
 			
 			//Buy clicker	
-			if (kDown & KEY_UP){
+			if (kDown & KEY_B){
 			
 				if(cookies >= buildingData[3]){
 					cookies-=buildingData[3];
@@ -86,7 +96,7 @@ int main()
 			}
 		
 			//Buy grandma
-			if( kDown & KEY_LEFT){
+			if( kDown & KEY_X){
 				if(cookies >= buildingData[4]){
 					cookies-=buildingData[4];
 					buildingData[4] = buildingData[4] * 1.2;
@@ -94,7 +104,7 @@ int main()
 				}
 			}
 		
-			if(kDown & KEY_RIGHT){
+			if(kDown & KEY_Y){
 				if(cookies >= buildingData[5]){
 					cookies-=buildingData[5];
 					buildingData[5] = buildingData[5] * 1.2;
@@ -108,6 +118,20 @@ int main()
 				fwrite(buildingData, sizeof(u64), 5, fp);
 				fclose(fp);
 			}
+			
+			if (kDown & KEY_DOWN) {
+				cursor--;
+			} else if (kDown & KEY_UP) {
+				cursor++;
+			}
+			
+			if (cursor < 12) {
+				cursor = 12;
+			} else if (cursor > 14) {
+				cursor = 14;
+			}
+			
+			
 		
 			consoleSelect(&bottomScreen);
 		
@@ -117,12 +141,12 @@ int main()
 			printf("\x1b[4;1HYou have %llu grandmas hired so far!\e[K\n", buildingData[1]);
 			printf("\x1b[5;1HYou have %llu bakeries built so far!\e[K\n", buildingData[2]);
 		
-			printf("\x1b[7;1HPress /\\ to buy a clicker");
-			printf("\x1b[8;1HPress <- to hire a grandma");
-			printf("\x1b[9;1HPress -> to produce a factory");
+			printf("\x1b[7;1HPress B to buy a clicker");
+			printf("\x1b[8;1HPress X to hire a grandma");
+			printf("\x1b[9;1HPress Y to produce a factory");
 			printf("\x1b[10;1HPress Select to Save");
 		
-		} else if (screen = 1) { //Select Shop
+		} else if (screen == 1) { //Select Shop
 			
 			consoleSelect(&topScreen);
 			
