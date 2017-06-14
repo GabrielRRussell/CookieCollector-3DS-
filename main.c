25f8cf154f5e28feec299b6ba39ee176ba35b924
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-13 21:52:43 -0400

    Oops, quick fix
    Compilation failure due to quick typing. Sorry.

--- main.c
+++ main.c
@@ -123,7 +123,7 @@ int main()
 			printf("\x1b[6;0H__________________________________________________\e[K\n");
 			printf("\x1b[8;0HPress B to go back\e[K\n");
 			
-			if (cursor > 4 & (buildingData[1] <= 25) {
+			if (cursor > 4 & (buildingData[1] <= 25)) {
 				cursor = 4;
 			} else if (cursor > 5) {
 				cursor = 5;
