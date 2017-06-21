786ad91d8c6d05bac0e40dc40c66b4fa8e6b07fa
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-21 16:44:38 -0400

    Fix some things

---
+++ download.c
@@ -0,0 +1,138 @@
+#include "download.h"
+#include <3ds.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int setupContext(httpcContext * context, const char * url, u32 * size)
+{
+	Result ret = 0;
+	u32 statuscode = 0;
+	
+	ret = httpcOpenContext(context, HTTPC_METHOD_GET, url, 1);
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	ret = httpcAddRequestHeaderField(context, "User-Agent", "MultiUpdater");
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	ret = httpcSetSSLOpt(context, SSLCOPT_DisableVerify);
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	ret = httpcAddRequestHeaderField(context, "Connection", "Keep-Alive");
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	ret = httpcBeginRequest(context);
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	ret = httpcGetResponseStatusCode(context, &statuscode);
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	if ((statuscode >= 301 && statuscode <= 303) || (statuscode >= 307 && statuscode <= 308)) {
+		char * newurl = malloc(0x1000); // One 4K page for new URL
+		if (newurl == NULL) {
+			httpcCloseContext(context);
+			return 2;
+		}
+		
+		ret = httpcGetResponseHeader(context, "Location", newurl, 0x1000);
+		if (ret != 0) {
+			httpcCloseContext(context);
+			free(newurl);
+			return ret;
+		}
+		
+		httpcCloseContext(context); // Close this context before we try the next
+		
+		if (newurl[0] == '/') { //if the url starts with a slash, it's local
+			int slashpos = 0;
+			char * domainname = strdup(url);
+			if (strncmp("http", domainname, 4) == 0) slashpos = 8; //if the url in the entry starts with http:// or https:// we need to skip that
+			slashpos = strchr(domainname+slashpos, '/')-domainname;
+			domainname[slashpos] = '\0'; // replace the slash with a nullbyte to cut the url
+			char * copyurl = strdup(newurl);
+			sprintf(newurl, "%s%s", domainname, copyurl);
+			free(copyurl);
+			free(domainname);
+		}
+		
+		ret = setupContext(context, newurl, size);
+		free(newurl);
+		return ret;
+	}
+	
+	if (statuscode != 200) {
+		httpcCloseContext(context);
+		return 3;
+	}
+	
+	ret = httpcGetDownloadSizeState(context, NULL, size);
+	if (ret != 0) {
+		httpcCloseContext(context);
+		return ret;
+	}
+	
+	return 0;
+}
+
+int downloadToFile(const char * url, const char * filepath)
+{
+	if (url == NULL) {
+		return 1;
+	}
+	
+	if (filepath == NULL) {
+		return 1;
+	}
+	
+	httpcContext context;
+	Result ret = 0;
+	u32 contentsize = 0, readsize = 0;
+	
+	ret = setupContext(&context, url, &contentsize);
+	if (ret != 0) return ret;
+	
+	FILE * fh = fopen(filepath, "wb");
+	if (fh == NULL) {
+		return 5;
+	}
+	
+	u8 * buf = malloc(0x1000);
+	if (buf == NULL) {
+		httpcCloseContext(&context);
+		return 3;
+	}
+	
+	u64 startTime = osGetTime();
+	do {
+		ret = httpcDownloadData(&context, buf, 0x1000, &readsize);
+		fwrite(buf, 1, readsize, fh);
+	} while (ret == (Result)HTTPC_RESULTCODE_DOWNLOADPENDING);
+	
+	free(buf);
+	fclose(fh);
+	httpcCloseContext(&context);
+	
+	if (ret != 0) {
+		return ret;
+	}
+	
+	return 0;
+}
---
+++ functions.c
@@ -0,0 +1,125 @@
+#include <string.h>
+#include <3ds.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+#include <sys/stat.h> 
+#include <sys/types.h> 
+
+void saveGame() {
+
+	FILE * fp = fopen("/3ds/data/cookiecollector/user.txt", "r+");
+	mkdir("/3ds/data/", 0777);
+	mkdir("/3ds/data/CookieCollector/", 0777);
+	
+	fwrite(&version, sizeof(int), 1, fp);
+	fwrite(&cookies, sizeof(u64), 1, fp);
+	fwrite(buildingTotal, sizeof(u64), 4, fp);
+	fwrite(buildingCost, sizeof(u64), 4, fp);
+	fwrite(upgradeCost, sizeof(u64), 6, fp);
+	fwrite(upgradeTotal, sizeof(u64), 6, fp);
+	fclose(fp);
+	
+}
+
+
+
+
+void moveCursor() {
+
+	if (hidKeysDown() & KEY_DOWN) {
+		cursor++;
+	} else if (hidKeysDown() & KEY_UP) {
+		cursor--;
+	} else if (hidKeysDown() & KEY_LEFT) {
+		cursor = cursor - 3;
+	} else if (hidKeysDown() & KEY_RIGHT) {
+		cursor = cursor + 3;
+	}
+}
+
+void resetCursor(int min, int max) {
+	if (cursor > max) {
+		cursor = max;
+	} else if (cursor < min) {
+		cursor = min;
+	}
+}
+
+void addCookies() {
+	cookies = cookies + (buildingTotal[0] * upgradeTotal[1]);
+	cookies = cookies + ((buildingTotal[1] * 5) * upgradeTotal[2]);
+	cookies = cookies + buildingTotal[2] * 15;
+	cookies = cookies + buildingTotal[3] * 50;
+}
+
+void loadGame() {
+	mkdir("/3ds/data/", 0777);
+	mkdir("/3ds/data/CookieCollector/", 0777);
+	FILE * fp = fopen("/3ds/data/cookiecollector/user.txt", "r+");
+	
+	if(!fp) { 
+		fp = fopen("/3ds/data/cookiecollector/user.txt", "w+");
+	}
+	
+	fread(&version, sizeof(int), 1, fp);
+	
+	if (version == 170) {
+		fread(&cookies, sizeof(u64), 1, fp);
+		fread(buildingTotal, sizeof(u64), 4, fp);
+		fread(buildingCost, sizeof(u64), 4, fp);
+		fread(upgradeCost, sizeof(u64), 6, fp);
+		fread(upgradeTotal, sizeof(u64), 6, fp);
+		rewind(fp);
+	} else {
+		fread(&cookies, sizeof(u64), 1, fp);
+		fread(buildingTotal, sizeof(u64), 4, fp);
+		fread(buildingCost, sizeof(u64), 4, fp);
+		fread(upgradeCost, sizeof(u64), 6, fp);
+		fread(upgradeTotal, sizeof(u64), 6, fp);
+		rewind(fp);
+	}
+	
+	version = 170;
+}
+
+void makeSale(u64* increment, u64* cost) {
+    if (cookies >= *cost) {
+        cookies -= *cost;
+        *cost *= 1.2;
+        *increment += 1;
+    }
+    
+}
+
+void printStatement(char* name, u64 cost, int place) {
+		
+	if (cursor == place) {
+		printf("\x1b[%d;2H\x1b[40;33m> \x1b[0m%s: %llu\e[K\n", place, name, cost);
+	} else {
+		printf("\x1b[%d;2H  %s: %llu\e[K\n", place, name, cost);
+	}
+}
+
+			
+void resetScreen() {
+	if (hidKeysDown() & KEY_B) {
+		screen = 0;
+		cursor = 0;
+		printf("\x1b[8;0H                   \e[K\n");
+		printf("\x1b[9;0H                   \e[K\n");
+		printf("\x1b[10;0H                   \e[K\n");
+		printf("\x1b[11;0H                   \e[K\n");
+		printf("\x1b[12;0H                   \e[K\n");
+		printf("\x1b[13;0H                   \e[K\n");
+		printf("\x1b[14;0H                   \e[K\n");
+		printf("\x1b[15;0H                   \e[K\n");
+		printf("\x1b[16;0H                   \e[K\n");
+		printf("\x1b[17;0H                   \e[K\n");
+		printf("\x1b[18;0H                   \e[K\n");
+		printf("\x1b[19;0H                   \e[K\n");
+
+		
+		consoleClear();			
+	}
+}
